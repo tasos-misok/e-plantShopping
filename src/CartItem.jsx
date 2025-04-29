31e02c119a6dd1bb3e783d00c6926d2447dfc53a
Anastasios Misokalos <tasosmis28@gmail.com>
2025-04-29 20:56:58 +0300

    all steps done

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    return cart
+        .reduce((total, item) => {
+            const price = parseFloat(item.cost.replace('$', ''));
+            return total + price * item.quantity;
+    },0)
+    .toFixed(2);
+    };
  
-  };
+
 
   const handleContinueShopping = (e) => {
-   
+    alert('Functionality to be added for future reference');
+    e.preventDefault();
+    onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }   
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.replace('$', ''));
+    return (price * item.quantity).toFixed(2);
   };
 
   return (
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,21 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const existingItem = state.items.find(item => item.name === action.payload.name);
+        if (!existingItem){
+            state.items.push({ ...action.payload, quantity:1 });
+        }
     },
     removeItem: (state, action) => {
+        const nameToRemove = action.payload;
+        state.items = state.items.filter(item => item.name !== nameToRemove);
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, quantity} = action.payload;
+        const item = state.items.find(item => item.name === name);
+        if (item){
+            item.quantity = quantity;
+        }
     },
   },
 });
